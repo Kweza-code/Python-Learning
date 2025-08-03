38100c24abdddde32e946fdb2d2995952218d916
Kweza <thomaslorilpro@gmail.com>
2025-08-03 12:41:26 +0200

    Adding lenght instruction on password

--- passwordSecure.py
+++ passwordSecure.py
@@ -5,6 +5,8 @@ def valid_password(password):
     has_upper = False
     special_char = 0
     number = 0
+    
+ 
 
     for char in password:
         if char.isupper():
@@ -13,8 +15,9 @@ def valid_password(password):
             special_char += 1
         if char.isdigit():
             number += 1
+        
 
-    if has_upper and special_char >= 3 and number >= 3:
+    if has_upper and special_char >= 3 and number >= 3 and len(password) > 6 :
         return True
     else:
         return False
@@ -25,7 +28,7 @@ def main():
 
     while True:
         password = getpass.getpass(
-            "Enter a password with 3 special characters, 3 numbers, and one uppercase letter: "
+            "Enter a password with 3 special characters, 3 numbers, one uppercase letter and lenght superior of 4: "
         )
         if valid_password(password):
             print("Password Valid")
